6aa74fe0c443769b3f7954f1caa5217a41d6e247
jeeunman <manjee.official@gmail.com>
2021-08-18 13:02:58 +0900

    Retrofit2 관련 모듈 생성

--- SearchGitRepository/app/src/main/java/com/manjee/searchgitrepository/di/AppModule.kt
+++ SearchGitRepository/app/src/main/java/com/manjee/searchgitrepository/di/AppModule.kt
@@ -1,10 +1,46 @@
 package com.manjee.searchgitrepository.di
 
+import com.manjee.searchgitrepository.api.UrlProvider
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideUrlProvider() = UrlProvider()
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .readTimeout(100, TimeUnit.SECONDS)
+            .connectTimeout(100, TimeUnit.SECONDS)
+            .addInterceptor(
+                HttpLoggingInterceptor()
+                    .setLevel(HttpLoggingInterceptor.Level.BODY)
+            ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideV1Retrofit(
+        okHttpClient: OkHttpClient,
+        urlProvider: UrlProvider
+    ): Retrofit =
+        Retrofit.Builder()
+            .baseUrl(urlProvider.getGitApiUrl())
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
 }
